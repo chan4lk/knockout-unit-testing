5ade5aa23e96da563f375285195a00e689779eab
Chandima Ranaaweera <chan4lk@gmail.com>
2016-11-04 21:45:25 +0530

    karma tests added

---
+++ test/viewModel.spec.js
@@ -0,0 +1,6 @@
+describe('Person Name', function(){
+    it('computes fullName based on firstName and lastName', function(){
+        var target = new PersonNameViewModel('Ada', 'Lovelace');
+        expect(target.fullName()).toBe('Ada Lovelace');
+    });
+});
