8223fbcfba4fcf83a9c66be7b214bd42a1ffe9fc
Chandima Ranaaweera <chan4lk@gmail.com>
2017-04-30 21:27:12 +0530

    typings updated.

--- src/query-builder.standalone.d.ts
+++ src/query-builder.standalone.d.ts
@@ -1,4 +1,33 @@
 interface JQuery  {
-    queryBuilder(op:any): JQuery;
-    QueryBuilderWrapper(): JQuery;
+
+    /**
+     * Create the query builder.
+     * 
+     * @param {any} options. 
+     * @returns {JQuery} 
+     * 
+     * @memberof JQuery
+     */
+    queryBuilder(options:any): JQuery;
+
+    /**
+     * Call the Query builder methods by name.
+     * 
+     * @param {string} method The method name. equalent to queryBuilder.[method]
+     * @param {*} options The method arguments.
+     * @returns {JQuery} 
+     * 
+     * @memberof JQuery
+     */
+    queryBuilder(method:string, ...options:any): JQuery;
+    
+    /**
+     * Create Query builder wrapper.
+     * 
+     * @param {*} options 
+     * @returns {JQuery} 
+     * 
+     * @memberof JQuery
+     */
+    QueryBuilderWrapper(options:any): JQuery;
 }
